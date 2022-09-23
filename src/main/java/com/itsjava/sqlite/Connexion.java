a4bd678aa6715fa731e7d57fee97bd2286670479
juman <ju@ju.com>
2022-09-23 14:19:01 +0200

    ajout de la lib SQLite

---
+++ src/main/java/com/itsjava/sqlite/Connexion.java
@@ -0,0 +1,14 @@
+package com.itsjava.sqlite;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class Connexion {
+
+    public static void testConnexion(){
+        Connection conn = null;
+        Statement stmt = null;
+
+        
+    }
+}
